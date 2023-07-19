2149d79c1d77906e1a22842107e89c58da6729ee
Elemental-Draco <jdwashington31@gmail.com>
2023-07-19 11:11:16 -0400

    added cpmments to app.jsx to guide some next steps.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import PokemonSlot from "./components/pokemon-slot";
 
 function App() {
+  // react use effect to call to the api and then perform separators for the randomiser
+  // use local storage to store every time the randomiser gets ran again, so when user reloads that team appears again
+  // track the state of each slot, or whole list of pokemon?
+
   return <PokemonSlot pokemon="shiftry" />;
 }
 
