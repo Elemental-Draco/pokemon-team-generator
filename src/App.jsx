e2a7934d7a275be0986ec4a7824933938ed20be0
Elemental-Draco <jdwashington31@gmail.com>
2023-07-18 22:51:37 -0400

    added full tailwind functionality, also added the component for each pokemon with a bit of styling. left comments to detail some things to work on next

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from "react";
+import PokemonSlot from "./components/pokemon-slot";
 
 function App() {
-  return <div>Hello world</div>;
+  return <PokemonSlot pokemon="shiftry" />;
 }
 
 export default App;
---
+++ src/components/pokemon-slot.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function PokemonSlot(props) {
+  // props.pokemon instead of hardcoded pikachu
+
+  const pokemonGif = `https://play.pokemonshowdown.com/sprites/ani/${props.pokemon}.gif`;
+
+  return (
+    <>
+      <div className=" flex flex-col ml-10 mt-10 w-[150px]">
+        <p className="justify-self-end">{props.pokemon}</p>
+
+        <div className="w-[150px] h-[150px] flex bg-red-700 items-center">
+          <img src={pokemonGif} alt="this pokemon" className="" />
+        </div>
+
+        <div className="w-full flex justify-between">
+          <button className="text-xl bg-cyan-600 rounded-xl p-2">
+            More info
+          </button>
+          <p className="text-2xl">🔒</p>
+        </div>
+      </div>
+    </>
+  );
+}
